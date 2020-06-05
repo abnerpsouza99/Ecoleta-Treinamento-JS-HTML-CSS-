ac7c4e6ea15a81243e7b3716451976b95f4a8b33
abnerpsouza99 <abnertecnicosenac@gmail.com>
2020-06-05 14:50:12 -0300

    Primeiro commit

---
+++ scripts/intro.js
@@ -0,0 +1,71 @@
+// coments
+// document.write("Hello")
+
+// variaveis, tipo de dados
+// Quando registramos uma variável sem informar valor, se torna "Undefined"
+// var myvar
+//myvar = "Hello "
+// const myconst = "Hi"
+// document.write(myvar + myconst)
+
+// string
+// const s1 = " Isso é uma string "
+// document.write(s1)
+
+// number
+// const n1 = 1
+// const n2 = 30
+//document.write(n1 + n2);
+
+// boolean = TRUE or FALSE
+//const bTrue = true
+//document.write(bTrue)
+
+// objet0 = todos os objetos possuem propriedades e funcionalidades
+// Para atribuir objetos se usa {}
+// const pessoa = {
+ // altura: "1,80m",
+ // idade: 24,
+ // solteiro: true,
+ // correr(){
+    // document.write("Run Forest")
+   // return "Run Forest"
+ // }
+// }
+// Utilizando "." acessamos a propriedade do objeto 
+// document.write(pessoa.correr())
+// Para executarmos uma função, não utilizamos document para chamá-la
+
+// Array (coleção de dados)
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "Abner"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções/Function/Atalhos
+
+// Registrar funcão
+ // function sayMyName(name){
+ // document.write(name)
+ //}
+
+// Execução da Função
+//sayMyName("Abner")
+//sayMyName("João")
+//sayMyName("Pedro")
+
+// Condicionais
+
+//const notaFinal = 7
+
+//if(notaFinal>5){
+  // document.write("Aprovado")
+// }else {
+//  document.write("Reprovado")
+// }
+
+// Loop de repetições
+
+// for(i = 0; i < 10; i++){
+  // Sintaxe para inserir variável js no print
+//  document.write(`<p>${i}</p>`)
+// }
